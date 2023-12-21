557cd6fd1c3a20b1118d56172af4f1a27d960331
monciego <jerichobantiquete@gmail.com>
2023-12-21 23:14:04 +0800

    feat: add post

--- x-laravel-livewire/routes/web.php
+++ x-laravel-livewire/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/', 'welcome');
+
+Route::get('posts', [PostController::class,'index'])
+    ->middleware('auth', 'verified')
+    ->name('posts');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
